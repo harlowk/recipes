c2e7b20ceeb19ff1a363bb6c024d2c337ca02c4a
harlowk <harlow.jkirk@gmail.com>
2022-11-30 18:54:11 -0500

    Define recipeMiddleware function

--- backend/src/routes/recipe.ts
+++ backend/src/routes/recipe.ts
@@ -1,9 +1,20 @@
 import { Request, Response, NextFunction } from "express"
+import { Recipe, RecipeModel } from "../models"
 
+/* 
+  Get a recipe by id
+  GET: /api/recipe/:id  => RecipeDetail | null
+*/
 export const recipeMiddleware = async (
   req: Request,
-  res: Response,
-  next: NextFunction
+  res: Response
 ): Promise<void> => {
-  // TODO fetch and return a recipe
+  const { id } = req.params
+  let recipe: Recipe | null = null
+  try {
+    recipe = (await RecipeModel.findById(id).exec()) || null
+  } catch (err) {
+    console.log("Recipe Issue: ", err)
+  }
+  res.send(recipe?.toDetail())
 }
