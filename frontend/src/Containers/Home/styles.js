6f47ffba3c4eb42e0cb8ee53e13311124877d5e6
harlowk <harlow.jkirk@gmail.com>
2022-11-30 18:58:26 -0500

    css styles

--- frontend/src/Containers/Home/styles.js
+++ frontend/src/Containers/Home/styles.js
@@ -7,4 +7,20 @@ export const HomeWrapper = styled.div`
   flex-flow: column;
   margin: auto;
   padding: 8px;
+  .overflow-container {
+    overflow: auto;
+    height: 20vh;
+    min-height: 20vh;
+    max-height: 20vh;
+  }
+  .recipe-selected {
+    .MuiTypography-body1 {
+      font-weight: 600;
+    }
+  }
+  .recipe-not-selected {
+    .MuiTypography-body1 {
+      font-weight: 400;
+    }
+  }
 `
