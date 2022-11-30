166795a6c022a838e614c4d8a3a917fa17ee290e
harlowk <harlow.jkirk@gmail.com>
2022-11-30 18:55:18 -0500

    add recipe redux actions

--- frontend/src/actions/recipe.js
+++ frontend/src/actions/recipe.js
@@ -1,3 +1,42 @@
-/* TODO: create recipe fetch actions, creators, and constants
-  API: use /api/recipe/:id as a get request to fetch the recipe info
-*/
+
+
+export const GET_RECIPE = "GET_RECIPE"
+export const RECEIVE_RECIPE = "RECEIVE_RECIPE"
+export const FAIL_RECIPE = "FAIL_RECIPE"
+
+const action = {
+  fetchingRecipe: () => ({
+    type: GET_RECIPE,
+  }),
+  fetchedRecipe: (payload) => ({
+    type: RECEIVE_RECIPE,
+    payload,
+  }),
+  failedRecipe: (payload) => ({
+    type: FAIL_RECIPE,
+    payload,
+  }),
+}
+
+export const executeRecipe = async (id) => {
+  const response = await fetch(`/api/recipe/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+  const recipe = await response.json()
+  return recipe
+}
+
+export const fetchRecipe = (id) => {
+  return async (dispatch) => {
+    dispatch(action.fetchingRecipe())
+    try {
+      const recipe = await executeRecipe(id)
+      dispatch(action.fetchedRecipe(recipe))
+    } catch (e) {
+      dispatch(action.failedRecipe(e))
+    }
+  }
+}
