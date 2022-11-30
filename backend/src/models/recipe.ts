e00d7ddeefae07d9c0ef4b6e4216921b08a12160
harlowk <harlow.jkirk@gmail.com>
2022-11-30 18:50:17 -0500

    Update Recipe schema/model to include converters

--- backend/src/models/recipe.ts
+++ backend/src/models/recipe.ts
@@ -1,23 +1,31 @@
 import { Document, Schema, Model, model } from "mongoose"
 import { Ingredient, IngredientSchema } from "./ingredient"
 
-export interface Recipe extends Document {
+export type RecipeTag = Pick<Recipe, "id" | "name">;
+export type RecipeDetail = Pick<Recipe, "name" | "instructions" | "ingredients">;
+export type Recipe = Document & {
+  id: string
   name: string
   instructions: string
   ingredients: Ingredient[]
+  toTag: () => RecipeTag
+  toDetail: () => RecipeDetail
 }
 
-const RecipeSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  instructions: {
-    type: String,
-    required: true,
-  },
+const RecipeSchema: Schema<Recipe> = new Schema({
+  name: { type: String, required: true, unique: true, },
+  instructions: { type: String, required: true, },
   ingredients: [IngredientSchema],
-})
+}, {
+  id: true
+});
 
-export const RecipeModel = model<Recipe, Model<Recipe>>("Recipe", RecipeSchema)
+RecipeSchema.methods.toTag = function () {
+  return { id: this.id, name: this.name }
+}
+
+RecipeSchema.methods.toDetail = function () {
+  return { name: this.name, instructions: this.instructions, ingredients: this.ingredients }
+}
+
+export const RecipeModel = model<Recipe, Model<Recipe>>("Recipe", RecipeSchema);
