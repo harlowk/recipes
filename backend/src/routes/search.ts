f653b8d4ca5e8300059bbca4fcac43b9947a8457
harlowk <harlow.jkirk@gmail.com>
2022-11-30 18:51:05 -0500

    Fix searchMiddleware issue

--- backend/src/routes/search.ts
+++ backend/src/routes/search.ts
@@ -1,20 +1,10 @@
-import { RecipeModel, Ingredient } from "../models"
 import { Request, Response } from "express"
+import { RecipeModel } from "../models"
 
 const allIngredients = ["flour", "sugar", "salt", "butter", "milk"]
 
-const escapeRegex = (text): string => {
+const escapeRegex = (text: string): string => {
   return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&")
-}
-
-interface Query {
-  name?: RegExp
-  ingredients?: Ingredient[]
-}
-
-const recipeCleaner = (recipe): { id: string; name: string } => {
-  const { id, name } = recipe
-  return { id, name }
 }
 
 export const searchMiddleware = async (
@@ -22,15 +12,11 @@ export const searchMiddleware = async (
   res: Response
 ): Promise<void> => {
   const { name, ingredients } = req.body
-  const query: Query = {}
-  if (name) {
-    query.name = new RegExp(escapeRegex(name), "gi")
-  }
-  if (ingredients) {
-    const whatsLeft = allIngredients.filter((ing) => !ingredients.includes(ing))
-    query["ingredients.name"] = { $nin: whatsLeft }
-  }
-  const foundRecipes = await RecipeModel.find(query)
-  const builtRecipes = foundRecipes.map(recipeCleaner)
-  res.send(builtRecipes)
+
+  const recipes = await RecipeModel
+    .find({ name: new RegExp(escapeRegex(name), "gi") })
+    .find({ "ingredients.name": { $nin: allIngredients.filter((ing) => !ingredients.includes(ing)) } })
+    .exec()
+
+  res.send(recipes.map((r) => r.toTag()))
 }
